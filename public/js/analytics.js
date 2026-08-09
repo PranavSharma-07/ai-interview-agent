cdf7aef3b772b523a6046eeec757948c1f464b7b
Pranav Sharma <pranavsharma07610761@gmail.com>
2026-08-09 17:54:18 +0530

    fix: return dashboard to landing page

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -30,7 +30,7 @@ function renderFeedback(feedback) {
 }
 
 document.querySelector("#back-button").addEventListener("click", () => {
-    window.location.href = document.referrer || "index.html";
+    window.location.href = document.referrer || "/";
 });
 
 const storedResult = readStoredJson("aiInterviewResult");
